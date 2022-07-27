48d1b4faae5530f808a77f7c1883a0089ecf15f9
Mikun07 <mikunolaleye@gmail.com>
2022-07-28 01:44:05 +0800

    complete onboarding screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ import AuthPin from './modules/auth/auth-pin/auth-pin.screen';
 import AuthResetPin from './modules/auth/auth-reset-pin/auth-reset-pin.screen';
 import Symble from './modules/onboarding/symble/symble.screen';
 import GettingStarted from './modules/onboarding/getting-started/getting-started.screem';
-
+import Screen1 from './modules/onboarding/screen1/screen';
+import Screen2 from './modules/onboarding/screen2/Screen2';
+import Screen3 from './modules/onboarding/screen3/screen3';
 
 function App() {
   return (
@@ -30,6 +32,12 @@ function App() {
           <Route exact path="/symble" component={Symble}/>
 
           <Route exact path="/gettingstarted" component={GettingStarted}/>
+
+          <Route exact path={"/onboardscreen1"} component={Screen1}/>
+
+          <Route exact path={"/onboardscreen2"} component={Screen2}/>
+
+          <Route exact path={"/onboardscreen3"} component={Screen3}/>
         {/* Routing for pages */}
         </Switch>
       </div> 
---
+++ src/modules/onboarding/screen1/screen.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Screenimg1 from "../../../assets/screen1.png"
+import Slider1 from "../../../assets/slider-1.svg"
+
+function Screen1() {
+    return (
+        <div className="container">
+            <img className="img-header" src={Screenimg1} alt="" />
+
+            <h1 className="screen1-text-1">
+                Share your event and receive money in real time!
+            </h1>
+            <h1 className="screen1-text-2">
+                You can invite people to join your live event through the app and give you monetary gifts in real time.
+            </h1>
+
+            <img className="slider-1" src={Slider1} alt="" />
+            <h1 className="screen1-text-3">
+                Next
+            </h1>
+        </div>
+    )
+}
+
+export default Screen1;
