49957785bea41f308d4b038867cbe775d39cc893
victorgbaye <victorgbayedev@gmail.com>
2022-07-31 13:21:13 +0100

    auth changes

--- src/modules/auth/auth-login/auth-login.screen.tsx
+++ src/modules/auth/auth-login/auth-login.screen.tsx
@@ -10,6 +10,7 @@ import { Link, useHistory} from 'react-router-dom';
 import { Button, Form, Input } from 'antd';
 import { field_rules } from './auth-login.helper';
 import { AuthContext } from '../auth.context';
+import './auth-login.styles.css'
 
 function AuthLogin() {
   const {setToken}:any = useContext(AuthContext)
@@ -17,6 +18,7 @@ function AuthLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("")
   const [emailError, setEmailError] = useState(false);
+
 
   const onSubmit = async (e:any) => {
     e.preventDefault()
@@ -39,66 +41,76 @@ function AuthLogin() {
     }
   };
   return (
-    <div className='container'>
-      {/* Header */}
-        <div className="auth-header">
-          <div className="symbleLogo">
-            <img src={symbleLogo} alt="Symble logo" />
+      <div className='page-container'>
+        {/* Header */}
+        <div >
+        <div className='auth-logo'>
+          <img src={symbleLogo} alt="Symble logo" />
           </div>
-          <h1 className='title'>
-            Let's sign in
+          <h1 className='auth-title'>
+          Let's sign in
           </h1>
-
-          <h1 className='red-text'>
-              Don't have an account?<span><Link className='a-text' to="/signup">Sign up</Link></span>
+          <h1 className='auth-grey-text'>
+          Don't have an account?<span><Link className='a-text' to="/signup">Sign up</Link></span>
           </h1>
         </div>
-      {/* Header */}
-
-      {/* form */}
-        <div className="auth-form">
-          <form>
-            <div>
-              <label className='user-label' htmlFor="Email Address/Username"> Email Address or Username</label>
-              <input className='user-input' type="text" placeholder='Email Address or Username' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-            </div>
-            <label className='password-label' htmlFor="Password">Password</label>
-            <input className='password-input' type="Password" placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)} autoComplete='new-password'/>
-            <Link to="/recovery"className='rev-text' href="">Forgot password?</Link>
-
-            <button className='btn-signin' type='button' onClick={onSubmit}>
-              <span className='btn-text'> Sign in </span>
-            </button>
-          </form>
+       <form>
+        <div className='input-container'>
+          <label className='input-label' htmlFor="Email">
+            Email address
+          </label>
+          <input 
+          placeholder='Enter email' 
+          type='email' 
+          className='input-field'
+          value={email} 
+          onChange={(e)=>setEmail(e.target.value)}
+          />
         </div>
-      {/* form */}  
+        <div className='input-container'>
+          <label
+          className='input-label'
+          htmlFor="Password"
+          >
+            Password
+          </label>
+          <input
+           className='input-field'
+           placeholder='Enter password'
+           type='password'
+           autoComplete='new-password'
+           value={password} 
+           onChange={(e)=>setPassword(e.target.value)}
 
+            />
+          <p className='forgot-password'>Forgot password?</p>
 
-      {/* Footer */}
-        
-        <div className="auth-footer">
-          {/* line */}
-            <hr className='log-line-left'/>
-                <span className='log-line-text'>or sign in with</span>
-            <hr className='log-line-right'/>
-          {/* line */}
-
-          {/* auth-botton */}
+        </div>
+        <button type='button' onClick={onSubmit} className='auth-cta'>
+          <span className='auth-btn-text'> Sign in </span>
+        </button>
+      </form>
+      {/* footer */}
+      <div>
+        <div className='auth-footer-title'>
+          <hr className='footer-line'/>
+          <p className='auth-footer-text'>or sign in with</p>
+          <hr className='footer-line'/>
+        </div>
+        <div className='socials'>
           <a href='https://spray-dev.herokuapp.com/api/auth/google/signin'>
-            <div className="cic-log-google">
-              <img className='log-google' src={Googleicon} alt=""/>
+            <div>
+            <img src={Googleicon} alt=""/>
             </div>
           </a>
-            <div className="cic-log-facebook">
-              <img className='log-facebook'src={Facebookicon} alt=""/>
-            </div>
-            <div className="cic-log-twitter">
-              <img className='log-twitter' src={Twittericon} alt=""/>
-            </div>
-          {/* auth-botton */}
+          <div>
+          <img src={Facebookicon} alt=""/>
+          </div>
+          <div>
+          <img src={Twittericon} alt=""/>
+          </div>
+        </div>
       </div>
-        
-      {/* Footer */}
     </div>
   )
 }
--- src/modules/auth/auth-signup/auth-signup.screen.tsx
+++ src/modules/auth/auth-signup/auth-signup.screen.tsx
@@ -7,6 +7,8 @@ import Twittericon from '../../../assets/Twitter.svg';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
+import './auth-signup.styles.css'
+
 
 
 export default function AuthSignup() {
@@ -46,60 +48,156 @@ export default function AuthSignup() {
     }
   };
   return (
-    <div className='container'>
-      {/* Header */}
-      <div className="auth-header">
-          <div className="symbleLogo">
-            <img src={symbleLogo} alt="Symble logo" />
-          </div>
-          <h1 className='title'>
-           Sign up
-          </h1>
+    // <div>
+    //   {/* Header */}
+    //   <div className="auth-header">
+    //       <div>
+    //         <img src={symbleLogo} alt="Symble logo" />
+    //       </div>
+    //       <h1 className='title'>
+    //        Sign up
+    //       </h1>
 
-          <h1 className='red-text'>
-              Already have an account?<span><Link className='a-text' to="/login">Sign in</Link></span>
-          </h1>
-        </div>
-      {/* Header */}
+    //       <h1 className='red-text'>
+    //           Already have an account?<span><Link className='a-text' to="/login">Sign in</Link></span>
+    //       </h1>
+    //     </div>
+    //   {/* Header */}
 
-      {/* form */}
-      <div className="auth-form">
-          <form>
+    //   {/* form */}
+    //   <div className="auth-form">
+    //       <form>
 
-            <label className='user-label' htmlFor="Email Address/Username">Email Address</label>
-            <input className='user-input' type="text" placeholder='Enter Email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+    //         <label className='user-label' htmlFor="Email Address/Username">Email Address</label>
+    //         <input className='user-input' type="text" placeholder='Enter Email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
 
-            <label className='sig-user-label' htmlFor="Username">Username</label>
-            <input className='sig-user-input' type="text" placeholder='Username' value={username} onChange={(e)=>setUsername(e.target.value)}/>
+    //         <label className='sig-user-label' htmlFor="Username">Username</label>
+    //         <input className='sig-user-input' type="text" placeholder='Username' value={username} onChange={(e)=>setUsername(e.target.value)}/>
 
-            <label className='sig-password-label-1' htmlFor="Password">Password</label>
-            <input className='sig-password-input-1' type="Password" placeholder='Enter Password' autoComplete='new-password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
-
-
-            <label className='sig-password-label-2' htmlFor="password2">Confirm Password</label>
-            <input className='sig-password-input-2' type="password" placeholder='Re-enter Password' value={password2} onChange={(e)=>setPassword2(e.target.value)}></input>
+    //         <label className='sig-password-label-1' htmlFor="Password">Password</label>
+    //         <input className='sig-password-input-1' type="Password" placeholder='Enter Password' autoComplete='new-password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
 
 
-            <button className='sig-btn-signin' type='button' onClick={onSubmit}>
-              <span className='sig-btn-text'> Sign in </span>
-            </button>
-          </form>
-        </div>
-      {/* form */}
+    //         <label className='sig-password-label-2' htmlFor="password2">Confirm Password</label>
+    //         <input className='sig-password-input-2' type="password" placeholder='Re-enter Password' value={password2} onChange={(e)=>setPassword2(e.target.value)}></input>
 
-      {/* line */}
-      <hr className='sig-line-left'/>
-          <span className='sig-line-text'>or sign up with</span>
-        <hr className='sig-line-right'/>
-      {/* line */}
 
-      {/* auth-botton */}
-      <a href="https://spray-dev.herokuapp.com/api/auth/google/signup">
-        <img className='sig-google' src={Googleicon} alt=""/>
-      </a>
-        <img className='sig-facebook'src={Facebookicon} alt=""/>
-        <img className='sig-twitter' src={Twittericon} alt=""/>
-      {/* auth-botton */}
+    //         <button className='sig-btn-signin' type='button' onClick={onSubmit}>
+    //           <span className='sig-btn-text'> Sign in </span>
+    //         </button>
+    //       </form>
+    //     </div>
+    //   {/* form */}
+
+    //   {/* line */}
+    //   <hr className='sig-line-left'/>
+    //       <span className='sig-line-text'>or sign up with</span>
+    //     <hr className='sig-line-right'/>
+    //   {/* line */}
+
+    //   {/* auth-botton */}
+    //   <a href="https://spray-dev.herokuapp.com/api/auth/google/signup">
+    //     <img className='sig-google' src={Googleicon} alt=""/>
+    //   </a>
+    //     <img className='sig-facebook'src={Facebookicon} alt=""/>
+    //     <img className='sig-twitter' src={Twittericon} alt=""/>
+    //   {/* auth-botton */}
+    // </div>
+    <div className='page-container'>
+    {/* Header */}
+    <div >
+    <div className='auth-logo'>
+      <img src={symbleLogo} alt="Symble logo" />
+      </div>
+      <h1 className='auth-title'>
+      Let's sign in
+      </h1>
+      <h1 className='auth-grey-text'>
+      Don't have an account?<span><Link className='a-text' to="/signup">Sign up</Link></span>
+      </h1>
     </div>
+   <form>
+    <div className='input-container'>
+      <label className='input-label' htmlFor="Email">
+        Email address
+      </label>
+      <input 
+      placeholder='Enter email' 
+      type='email' 
+      className='input-field'
+      value={email} 
+      onChange={(e)=>setEmail(e.target.value)}
+      />
+    </div>
+    <div className='input-container'>
+      <label className='input-label' htmlFor="Email">
+        Username
+      </label>
+      <input 
+      placeholder='Enter username' 
+      type='email' 
+      className='input-field'
+      value={username} 
+      onChange={(e)=>setUsername(e.target.value)}
+      />
+    </div>
+    <div className='input-container'>
+      <label
+      className='input-label'
+      htmlFor="Password"
+      >
+        Password
+      </label>
+      <input
+       className='input-field'
+       placeholder='Enter password'
+       type='password'
+       autoComplete='new-password'
+       value={password} 
+       onChange={(e)=>setPassword(e.target.value)}
+      /> 
+    </div>
+    <div className='input-container'>
+      <label
+      className='input-label'
+      htmlFor="Password"
+      >
+        Confirm Password
+      </label>
+      <input
+       className='input-field'
+       placeholder='Re-enter password'
+       type='password'
+       autoComplete='new-password'
+       value={password} 
+       onChange={(e)=>setPassword(e.target.value)}
+      /> 
+    </div>
+    <button type='button' onClick={onSubmit} className='auth-cta'>
+      <span className='auth-btn-text'> Sign in </span>
+    </button>
+  </form>
+  {/* footer */}
+  <div>
+    <div className='auth-footer-title'>
+      <hr className='footer-line'/>
+      <p className='auth-footer-text'>or sign in with</p>
+      <hr className='footer-line'/>
+    </div>
+    <div className='socials'>
+      <a href='https://spray-dev.herokuapp.com/api/auth/google/signin'>
+        <div>
+        <img src={Googleicon} alt=""/>
+        </div>
+      </a>
+      <div>
+      <img src={Facebookicon} alt=""/>
+      </div>
+      <div>
+      <img src={Twittericon} alt=""/>
+      </div>
+    </div>
+  </div>
+</div>
   )
 }
